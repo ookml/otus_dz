5c14dfc98c63e1913553197f1a0c2caa7e305daa
ookml <400kgkml@gmail.com>
2024-02-05 15:15:41 +0300

    Update config.sh

--- ZFS/config.sh
+++ ZFS/config.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+#install zfs repo
+          yum -y update
+          yum install -y http://download.zfsonlinux.org/epel/zfs-release.el7_8.noarch.rpm
+          #import gpg key 
+          rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-zfsonlinux
+          #install DKMS style packages for correct work ZFS
+          yum install -y epel-release kernel-devel zfs
+          #change ZFS repo
+          yum-config-manager --disable zfs
+          yum-config-manager --enable zfs-kmod
+          yum install -y zfs
+          #Add kernel module zfs
+          modprobe zfs
+          #install wget
+          yum install -y wget
+
+
+
 # Create pools RAID 1
 zpool create otus1 mirror /dev/sdb /dev/sdc
 zpool create otus2 mirror /dev/sdd /dev/sde
