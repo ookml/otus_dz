1d862c9fda7548e0f29debaa31bfdabbe6efb799
ookml <400kgkml@gmail.com>
2024-05-28 09:53:04 +0300

    Add files via upload

---
+++ VPN/tap/client.sh
@@ -0,0 +1,16 @@
+cat <<EOF > /etc/openvpn/server.conf 
+dev tun
+remote 192.168.10.10
+ifconfig 10.10.10.2 255.255.255.0
+topology subnet
+route 192.168.10.0 255.255.255.0
+secret /etc/openvpn/static.key
+comp-lzo
+status /var/log/openvpn-status.log
+log /var/log/openvpn.log
+verb 3
+EOF
+ssh-keyscan -H 192.168.10.10 >> /root/.ssh/known_hosts
+sshpass -p 'vagrant' scp root@192.168.10.10:/etc/openvpn/static.key /etc/openvpn/
+systemctl start openvpn@server
+systemctl enable openvpn@server
